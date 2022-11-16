84a775c473c20574ad8d88a8454654743c39fde4
Anna <ankakras@yandex.ru>
2022-11-16 13:19:00 +0700

    Решение

---
+++ Task001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+string NumbersMLessN(int m, int n)
+{
+    if(m <= n) return $"{m} " + NumbersMLessN(m + 1, n);
+    else return String.Empty;
+}
+string NumbersMMoreN(int m, int n)
+{
+    if(m >= n) return $"{m} " + NumbersMMoreN(m - 1, n);
+    else return String.Empty;
+}
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+if(m < n)
+    Console.WriteLine(NumbersMLessN(m, n));
+else if(m > n)
+    Console.WriteLine(NumbersMMoreN(m, n));
+else
+    Console.WriteLine(); 
