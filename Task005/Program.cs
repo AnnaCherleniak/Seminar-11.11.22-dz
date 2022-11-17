ec925d20cda81b7e7e3f4913013f2f2d74c26d66
Anna <ankakras@yandex.ru>
2022-11-17 15:29:38 +0700

    Решение

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -2,29 +2,26 @@
 // для которых каждое следующее равно сумме двух предыдущих. 
 // Первые два элемента последовательности задаются пользователем
 
-int Sum(int a, int b)
+int Sum(int a, int b, int n)
 {
-    return a + b;
-
+    if(n == 1) 
+        return a;
+    else if(n == 2)
+        return b;
+    else if(n == 3)
+        return a + b;
+    else
+        return Sum(b, a + b, n - 1); 
 }
 Console.WriteLine("Введите первый элемент последовательности: ");
 int a = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите второй элемент последовательности: ");
 int b = int.Parse(Console.ReadLine() ?? "0");
-int n = 10;
+int n = 8;
 Console.WriteLine();
-/*for(int i = 1; i < n; i++)
-{
-    int temp = a + b;
-    Console.WriteLine(temp);
-    a = b;
-    b = temp;
-}*/
 
 for(int i = 1; i <= n; i++)
 {
-    Console.WriteLine($"{i}  {Sum(a, b)}");
-    int temp = b;
-    b = Sum(a, b);
-    a = temp;
+    Console.WriteLine($"{i}  {Sum(a, b, i)}");
 }
+
